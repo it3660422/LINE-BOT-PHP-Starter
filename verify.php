3c73ab41b09d333772c9535fc8400dbde2241a44
it3660422 <it3660422@gmail.com>
2017-10-24 21:08:08 +0700

    Add files via upload

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
+$access_token = 'qB4SGXnXb+lZ2zf1SUPwBoBwfuuUKD245XJ+M2zJ5ifGkswr1twudJg0kfr1+MVvVljlpArF+CaEJe3oN/TxiaQS+ubBUR/gs26RbJgLjQ1A50ItoZzmtt3oQvnIMWu+HVx5at5Jd5w27iwiN2UuagdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
