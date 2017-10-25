811e6576469cb4b10b8dd7a83fcac9be0fb29129
it3660422 <it3660422@gmail.com>
2017-10-25 16:34:06 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -15,14 +15,22 @@ if (!is_null($events['events'])) {
 			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
+			
 			// Build message to reply back
-
+			$msg = 'Criteria is not match!!!'
 			if (strtolower($text)==strtolower('BTC')){
 				$btcPrice = btcPrice();
-			}
+				$msg ='Current BTC price is: '.$btcPrice
+			} 
+
+
+
+
+
+
 			$messages = [
 				'type' => 'text',
-				'text' => 'Current BTC price is: '.$btcPrice
+				'text' => $msg
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
