1939ea21e4879547c7a102cc7cc542bcb16a2e8c
it3660422 <it3660422@gmail.com>
2017-10-25 16:18:45 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -1,6 +1,5 @@
 <?php
 $access_token = 'qB4SGXnXb+lZ2zf1SUPwBoBwfuuUKD245XJ+M2zJ5ifGkswr1twudJg0kfr1+MVvVljlpArF+CaEJe3oN/TxiaQS+ubBUR/gs26RbJgLjQ1A50ItoZzmtt3oQvnIMWu+HVx5at5Jd5w27iwiN2UuagdB04t89/1O/w1cDnyilFU=';
-
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
@@ -16,13 +15,11 @@ if (!is_null($events['events'])) {
 			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
-
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
 				'text' => 'KakKAK'
 			];
-
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
@@ -31,7 +28,6 @@ if (!is_null($events['events'])) {
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
 			$ch = curl_init($url);
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -40,9 +36,8 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 			$result = curl_exec($ch);
 			curl_close($ch);
-
 			echo $result . "\r\n";
 		}
 	}
 }
-echo "OK";
+echo $result;
